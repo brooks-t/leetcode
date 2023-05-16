c2c1da07c999ea8f8a312c9be8d36a31787ae1a9
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2023-05-16 11:21:24 -0400

    solved p1 two sum challenge and added a refactor with gpt

---
+++ p-01-two-sum.js
@@ -0,0 +1,37 @@
+// 1. Two Sum: https://leetcode.com/problems/two-sum/
+// Given an array of integer nums and an integer target, return indices of the two numbers such that they add up to target.
+const problemTitle = document.getElementById('problemTitle');
+problemTitle.innerHTML = '1. Two Sum';
+
+console.log("----------my solution---------");
+const twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+console.log(twoSum([10,7,2,15], 9));
+
+// refactored solution via chatGPT
+console.log("----------refactored solution via chatGPT---------");
+const twoSumRefactor = function (nums, target) {
+	const numMap = new Map();
+	console.log(numMap);
+	for (let i = 0; i < nums.length; i++) {
+		const complement = target - nums[i];
+
+		if (numMap.has(complement)) {
+			return [numMap.get(complement), i];
+		}
+
+		numMap.set(nums[i], i);
+	}
+
+	return [];
+};
+
+console.log(twoSumRefactor([10, 7, 2, 15], 9));
